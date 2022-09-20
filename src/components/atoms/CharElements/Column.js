9f9e18793ce53238ff719c0bdc3847fb252f3058
Patryk Kuzdrowski <patryk.kuzdrowski@gmail.com>
2022-09-20 20:04:19 +0200

    Fixed column state, active on hover

--- src/components/atoms/CharElements/Column.js
+++ src/components/atoms/CharElements/Column.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Subtitle,
   ColumnWrapper,
+  Column as Height,
+  AmountInfo,
 } from "./Column.styles";
 
-export const Column = (props) => {
-  <ColumnWrapper>
-  {console.log(props)}
-    <Subtitle>{props.day}</Subtitle>
-    {/* <Height amount={props.amount} biggestValue={props.biggestValue} /> */}
-  </ColumnWrapper>;
+export const Column = ({day, amount, biggestValue}) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+    console.log(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+    console.log(false);
+  };
+  return (
+    <ColumnWrapper onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <Subtitle>{day}</Subtitle>
+      <Height amount={amount} biggestValue={biggestValue} isHovering={isHovering}/>
+      {isHovering && <AmountInfo>${amount}</AmountInfo>}
+    </ColumnWrapper>
+  );
 };
--- src/components/atoms/CharElements/Column.styles.js
+++ src/components/atoms/CharElements/Column.styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Column = styled.div`
   background-color: ${({biggestValue, amount, theme}) => biggestValue === amount ? theme.colors.lightBlue : theme.colors.orange};
+  opacity: ${({isHovering}) => isHovering ? '.5' : '1'};
   width: 30px;
   margin: 6px;
   height: ${({ amount }) => amount * 2.5}px;
@@ -17,4 +18,17 @@ export const ColumnWrapper = styled.div`
   display: flex;
   flex-direction: column-reverse;
   align-items: center;
+`;
+
+export const AmountInfo = styled.div`
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.dark};
+  font-size: ${({ theme }) => theme.fontSize.s};
+  border-radius: 5px;
+  height: 23px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
 `;
